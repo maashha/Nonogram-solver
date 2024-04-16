7755a6b3ca01ee9d143aeff1ebc84c2f9be7debf
maashha <72510050+maashha@users.noreply.github.com>
2024-04-16 04:58:29 +0300

    Update efficiency.py

--- efficiency.py
+++ efficiency.py
@@ -1,13 +1,8 @@
 from Nonogram_solver import efficiency_counter
 from Nonogram_project import NonogramSolver
-from backtracking_naive import efficiency_naive
 from enumerative_backtracking_solver import efficiency_enumerative
 import pandas as pd
 import time
-from multiprocessing import Process
-import multiprocessing
-from concurrent.futures import ThreadPoolExecutor
-from threading import Thread
 
 existing_file = 'Efficiency_comparison.xlsx'
 tests = [70419, 70402, 70399, 70472, 70449, 70446, 70474, 70470, 70468, 70442, 70281, 70260, 70289, 70070, 69690]
